f9e47ceb964b18ac2c4d2f05cda2a21f1b7ef28b
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2023-02-23 23:15:12 +0100

    feat: add display notification in fore and background

--- App.tsx
+++ App.tsx
@@ -1,10 +1,74 @@
-import { Text, View } from 'react-native'
+import { useEffect } from 'react'
+import { Button, Text, TouchableOpacity, View } from 'react-native'
 import { styles } from './styles'
+import notifee, { AndroidImportance, EventType } from '@notifee/react-native'
 
 export default function App() {
+  const createChannel = async () => {
+    const channelId = await notifee.createChannel({
+      id: 'test',
+      name: 'sales',
+      importance: AndroidImportance.HIGH,
+      vibration: true
+    })
+    return channelId
+  }
+
+  const displayNotification = async () => {
+    await notifee.requestPermission()
+    const channelId = await createChannel()
+
+    await notifee.displayNotification({
+      id: '123',
+      title: 'Olá, <strong>Notifee131313</strong>🤩!',
+      body: 'Hello World!',
+      android: {
+        channelId: channelId
+      },
+      ios: {
+        sound: 'default'
+      }
+    })
+  }
+
+  useEffect(() => {
+    return notifee.onForegroundEvent(({ type, detail }) => {
+      switch (type) {
+        case EventType.DISMISSED:
+          console.log('Notification dismissed', detail.notification)
+          break
+        case EventType.PRESS:
+          console.log('Notification pressed', detail.notification)
+          break
+      }
+    })
+  }, [])
+
+  useEffect(() => {
+    return notifee.onBackgroundEvent(async ({ type, detail }) => {
+      switch (type) {
+        case EventType.DISMISSED:
+          console.log('Notification dismissed', detail.notification)
+          break
+        case EventType.PRESS:
+          console.log('Notification pressed', detail.notification)
+          break
+      }
+    })
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Local notifications</Text>
+      <Text style={styles.title}>Local notifications</Text>
+      <View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={displayNotification}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Display notification</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- styles.ts
+++ styles.ts
@@ -5,6 +5,20 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-around'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center'
+  },
+  button: {
+    backgroundColor: '#005eff',
+    padding: 10,
+    borderRadius: 5,
+    margin: 10
+  },
+  buttonText: {
+    color: '#fff'
   }
 })
